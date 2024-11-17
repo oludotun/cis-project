80189965c22cdc13fd82b54ebe402b3f3e626ed4
Oludotun Ebiekuraju <dotun@intuitigo.com>
2024-11-17 19:53:16 +0100

    Updated the app/page file

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import Slider from "@/components/Slider";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+        <main className="">
+            <Navbar />
+            <Slider />
+        </main>
     );
 }
